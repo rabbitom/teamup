123368d69ab76b12ce1332eb0e0cf200d6292ebb
erabbit <rabbitom@gmail.com>
2017-07-23 02:13:15 +0800

    完成了创意页面

--- pages/ideas/ideas.js
+++ pages/ideas/ideas.js
@@ -5,7 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    ideas: [
+      {
+        title: "Chat app about hobbies",
+        memberCount: 5,
+        skills: ["iOS", "Android", "Backend"],
+        introduction: "Users would sign up, choose their hobbies, find people with same ones, chat and make date for offline events."
+      },
+      {
+        title: "Mechine learning of traffic",
+        memberCount: 1,
+        skills: ["Python", "BigData"],
+        introduction: "Analysis realtime traffic information and make route suggestion with meachine learning."
+      },
+      {
+        title: "IoT Sensor platform",
+        memberCount: 2,
+        skills: ["Embedded", "Architect"],
+        introduction: "Build an platform to gather data from massive IoT sensors around the world. The key to this is a highly scalable cloud infrastructure."
+      }
+    ]
   },
 
   /**
