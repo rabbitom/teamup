fb619789a19dda664e0b3da84f0965a47f12660c
erabbit <rabbitom@gmail.com>
2017-07-23 10:55:33 +0800

    过滤社区成员和创意

--- pages/community/community.js
+++ pages/community/community.js
@@ -5,9 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
+    filter: "",
     selectedIndex: -1,
     people: [
       {
+        show: true,
         name: "Tom",
         avatar: "https://wx.qlogo.cn/mmopen/vi_32/rX2Oiaqib2QGM5jBCLNUJLribFl6gp6LSqQN2IGTd8dPViaH5gicYUNINhbqicNLnSnSeZVrV8yFq50nkKUQ0bWYJFIw/0",
         skills: ["iOS", "Android", "AWS"],
@@ -15,12 +17,14 @@ Page({
         introduction: "I'm a developer focusing on IoT applications. I'm intrested in cross-platform developing. My favorite programming languages are JavaScript and C#."
       },
       {
+        show: true,
         name: "Jerry",
         avatar: "http://up.qqjia.com/z/face01/face06/facejunyong/junyong27.jpg",
         skills: ["Node.js", "Angular"],
         introduction: "I'm a JavaScript full-stack engineer. I do backend and frontend at same time. Looking for something cool to build!"
       },
       {
+        show: true,
         name: "Donald",
         avatar: "http://sc.admin5.com/uploads/allimg/100210/113629E45-2.png",
         skills: ["UI/UX"],
@@ -28,12 +32,43 @@ Page({
         introduction: "I'm a designer. I can help if someone need UI/UX designing. And I do have an idea about building an app for people to chat about their hobbies. Feel free to message me if you are intrested."
       },
       {
+        show: true,
         name: "Jack",
         avatar: "http://up.qqjia.com/z/face01/face06/facejunyong/junyong02.jpg",
         skills: ["Python", "AI"],
         introduction: "I'm a developer, focusing on IoT applications. I'm intrested in cross-platform developing. My favorite programming languages are JavaScript and C#."
       }
     ]
+  },
+
+  removeFilter: function() {
+    for (var person of this.data.people)
+      person.show = true;
+    this.setData({ people: this.data.people });
+  },
+
+  onFilterChange: function(event) {
+    var filter = event.detail.value;
+    if ((filter !== undefined) && (filter.length > 0)) {
+      filter = filter.toLowerCase();
+      for (var person of this.data.people) {
+        var show = false;
+        for (var skill of person.skills)
+          if (skill.toLowerCase().indexOf(filter) >= 0) {
+            show = true;
+            break;
+          }
+        person.show = show;
+      }
+      this.setData({ people: this.data.people });
+    }
+    else
+      this.removeFilter();
+  },
+
+  onFilterClear: function(event) {
+    this.setData({filter:""});
+    this.removeFilter();
   },
 
   onClickPerson: function(event) {
--- pages/ideas/ideas.js
+++ pages/ideas/ideas.js
@@ -8,24 +8,57 @@ Page({
     selectedIndex: -1,
     ideas: [
       {
+        show: true,
         title: "Chat app about hobbies",
         memberCount: 5,
         skills: ["iOS", "Android", "Backend"],
         introduction: "Users would sign up, choose their hobbies, find people with same ones, chat and make date for offline events."
       },
       {
+        show: true,
         title: "Mechine learning of traffic",
         memberCount: 1,
         skills: ["Python", "BigData"],
         introduction: "Analysis realtime traffic information and make route suggestion with meachine learning."
       },
       {
+        show: true,
         title: "IoT Sensor platform",
         memberCount: 2,
         skills: ["Embedded", "Architect"],
         introduction: "Build an platform to gather data from massive IoT sensors around the world. The key to this is a highly scalable cloud infrastructure."
       }
     ]
+  },
+
+  removeFilter: function () {
+    for (var idea of this.data.ideas)
+      idea.show = true;
+    this.setData({ ideas: this.data.ideas });
+  },
+
+  onFilterChange: function (event) {
+    var filter = event.detail.value;
+    if ((filter !== undefined) && (filter.length > 0)) {
+      filter = filter.toLowerCase();
+      for (var idea of this.data.ideas) {
+        var show = false;
+        for (var skill of idea.skills)
+          if (skill.toLowerCase().indexOf(filter) >= 0) {
+            show = true;
+            break;
+          }
+        idea.show = show;
+      }
+      this.setData({ ideas: this.data.ideas });
+    }
+    else
+      this.removeFilter();
+  },
+
+  onFilterClear: function (event) {
+    this.setData({ filter: "" });
+    this.removeFilter();
   },
 
   onClickIdea: function(event) {
